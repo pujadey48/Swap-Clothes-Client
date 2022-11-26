a1ffa127e743af235e901ac9436302ded10f9635
Puja Dey <pujadey48@gmail.com>
2022-11-27 03:19:12 +0600

    product delete otion added

--- src/pages/Dashboard/Myproducts/MyProducts.js
+++ src/pages/Dashboard/Myproducts/MyProducts.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getUrl } from "../../../Util/Util";
+import Table from 'react-bootstrap/Table';
+import { Link } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const MyProducts = () => {
   const { data: myproducts = [], refetch } = useQuery({
@@ -21,13 +25,74 @@ const MyProducts = () => {
     refetch();
   }, []);
 
+  const showDeleteToastMessage = () => {
+    toast.success("Successfully deleted!", {
+      position: toast.POSITION.TOP_RIGHT,
+    });
+  };
+
+  const deleteProduct= (id)=>{
+    const proceed = window.confirm(
+        "Are you sure, you want to delete this review?"
+      );
+      if (proceed) {
+        fetch(getUrl(`/product/${id}`), {
+          method: "DELETE",
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("jwt-token")}`,
+          },
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+                refetch();
+                showDeleteToastMessage();
+            }
+          });
+      }
+  }
+
   return (
     <div>
       <h2>My Products</h2>
-
-      {myproducts.map((product) => (
-        <h3>{product.name}</h3>
+      <ToastContainer />
+      <Table striped>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Selling Price</th>
+          <th>Status</th>
+          <th>Delete</th>
+          <th>Advertise</th>
+        </tr>
+      </thead>
+      <tbody>
+      {myproducts.map((product, index) => (
+          <tr>
+          <td>{index}</td>
+          <td>{product.name}</td>
+          <td>{product.selling_price}</td>
+          <td>{product.status}</td>
+          <td><Link onClick={()=>{deleteProduct(product._id)}}>Delete</Link></td>
+          <td>@mdo</td>
+        </tr>
       ))}
+        
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+      
     </div>
   );
 };
